c7260777a81b16e24a5de277bc27bd81a4a03392
jude_koffi_repo <Koffijude33@gmail.com>
2024-08-25 23:57:10 +0000

    update center seeder

--- database/seeders/Center.php
+++ database/seeders/Center.php
@@ -20,55 +20,55 @@ class Center extends Seeder
             'center_category_id' => 6
         ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Chrysalide',
-            'center_category_id' => 1
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Chrysalide',
+        //     'center_category_id' => 1
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Angré',
-            'center_category_id' => 2
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Angré',
+        //     'center_category_id' => 2
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Cocody',
-            'center_category_id' => 2
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Cocody',
+        //     'center_category_id' => 2
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Treichville',
-            'center_category_id' => 2
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Treichville',
+        //     'center_category_id' => 2
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Yopougon',
-            'center_category_id' => 2
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Yopougon',
+        //     'center_category_id' => 2
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Toit Rouge',
-            'center_category_id' => 3
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Toit Rouge',
+        //     'center_category_id' => 3
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Gendarmerie',
-            'center_category_id' => 4
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Gendarmerie',
+        //     'center_category_id' => 4
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Grand Centre',
-            'center_category_id' => 1
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Grand Centre',
+        //     'center_category_id' => 1
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Yopougon Attié',
-            'center_category_id' => 5
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Yopougon Attié',
+        //     'center_category_id' => 5
+        // ]);
 
-        DB::table('centers')->insert([
-            'name' => 'Iroko',
-            'center_category_id' => 1
-        ]);
+        // DB::table('centers')->insert([
+        //     'name' => 'Iroko',
+        //     'center_category_id' => 1
+        // ]);
 
         DB::table('centers')->insert([
             'name' => 'Externe',
